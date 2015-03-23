ed16d2880ae4e9b17bfc1ec5fa0d1a9b8af123c0
PDUC <danielur3@gmail.com>
2015-03-23 13:07:15 -0600

    Simple Canvas draw line working!

--- app/src/main/java/com/example/pduc/edm_rapid_cam/MainActivity.java
+++ app/src/main/java/com/example/pduc/edm_rapid_cam/MainActivity.java
@@ -1,39 +1,31 @@
 package com.example.pduc.edm_rapid_cam;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
-
-public class MainActivity extends ActionBarActivity {
-
+@SuppressWarnings("ALL")
+public class MainActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#CD5C5C"));
+        paint.setStrokeWidth(12);
+        Bitmap bg = Bitmap.createBitmap(1200, 1800, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bg);
+        canvas.drawLine(50, 1510, 1100, 200, paint);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.rect);
+        ll.setBackgroundDrawable(new BitmapDrawable(bg));
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
+
