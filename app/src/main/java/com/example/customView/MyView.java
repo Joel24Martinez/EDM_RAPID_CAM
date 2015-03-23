c028e7b3ad858fc6aed706c9faa86a4f56a35e09
PDUC <danielur3@gmail.com>
2015-03-23 17:53:50 -0600

    Custom view, touch draw circle, working!

--- app/src/main/java/com/example/customView/MyView.java
+++ app/src/main/java/com/example/customView/MyView.java
@@ -1,6 +1,7 @@
 package com.example.customView;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -8,10 +9,15 @@ import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.pduc.edm_rapid_cam.R;
 
 public class MyView extends View {
     private PointF point;
     private Paint paint;
+    private Paint text;
+    boolean erase = false;
 
     public MyView(Context context) {
         this(context, null);
@@ -20,15 +26,34 @@ public class MyView extends View {
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        text = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (point != null) {
-            int radius = 50;
-            paint.setColor(Color.GREEN);
-            canvas.drawCircle(point.x, point.y, radius, paint);
-        }
+
+            int thickness = 30;
+            paint.setColor(Color.BLACK);
+            text.setColor(Color.BLACK);
+            paint.setStrokeWidth(thickness);
+            text.setTextSize(30);
+            Bitmap bg = Bitmap.createBitmap(1200, 1200, Bitmap.Config.ARGB_8888);
+            canvas.drawLine(300, 100, 900, 100, paint);
+            canvas.drawLine(600, 100, 600, 900, paint);
+            canvas.drawText("L1 [ mm/1000 ]", 500, 50, text);
+            canvas.drawText("L2 [ mm/1000 ]", 360, 500, text);
+
+
+
+        if (erase==true){
+            canvas.drawColor(Color.WHITE);}
+
+
+    }
+
+  /*public void Yshapedclicked(View view) {
+      erase=true;
+      invalidate();
     }
 
     @Override
@@ -47,5 +72,5 @@ public class MyView extends View {
         }
         invalidate();
         return true;
-    }
+    }*/
 }
--- app/src/main/java/com/example/pduc/edm_rapid_cam/MainActivity.java
+++ app/src/main/java/com/example/pduc/edm_rapid_cam/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.pduc.edm_rapid_cam;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.customView.MyView;
 
 public class MainActivity extends Activity {
+
+    private MyView Mycanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
